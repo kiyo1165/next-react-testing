6edf675adeb6f64e88d9c91db1dc2e4ff915911d
akashi <kiyotsugu.send@gmail.com>
2022-02-28 18:37:11 +0900

    fix error

--- pages/task-page.tsx
+++ pages/task-page.tsx
@@ -21,7 +21,7 @@ const TaskPage: React.FC<STATICPROPS> = ({ staticTasks }) => {
     fallbackData: staticTasks, //SWRの初期データをセット
     revalidateOnMount: true,
   })
-  if (error) return <span>Eror!</span>
+  if (error) return <span>Error!</span>
 
   return (
     <Layout title="Todos">
